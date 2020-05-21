14d988c1ccbed94c0eb418314e1551869c49e1a8
jakubriegel <jakubriegel112@gmail.com>
2020-05-21 19:19:11 +0200

    client | using data from dataset

--- sample-client/client/asynchronous/profiles.py
+++ sample-client/client/asynchronous/profiles.py
@@ -8,86 +8,92 @@ import json
 from itertools import groupby
 from multiprocessing import Queue
 import csv
+from requests import get
+
+
+_BS_URL = 'http://localhost:80/'
 
 
 def _genres() -> Dict[int, List[str]]:
     with open('../../data/movie_genres.csv') as genres_file:
         reader = csv.reader(genres_file, delimiter='\t')
         next(reader)
-        return {movie_id: list(map(lambda g: g[1], genres)) for movie_id, genres in groupby(reader, key=lambda r: r[0])}
+        return {int(movie_id): list(map(lambda g: g[1].lower().replace('-', '_'), genres)) for movie_id, genres in groupby(reader, key=lambda r: r[0])}
 
 
 def _ratings() -> List[tuple]:
     with open('../../data/user_ratedmovies-timestamps.csv') as ratings_file:
         reader = csv.reader(ratings_file, delimiter='\t')
         next(reader)
-        return sorted(map(tuple, reader), key=lambda i: i[3], reverse=True)
+        return sorted(map(lambda row: (int(row[0]), int(row[1]), float(row[2]), int(row[3])), map(tuple, reader)), key=lambda i: i[3], reverse=True)
 
 
-_GENRES = _genres()
-_RATINGS = _ratings()
+_MOVIES_GENRES = _genres()
+_USER_RATED_MOVIES = _ratings()
 _EXCHANGE = 'bs_update'
 _connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 _channel = _connection.channel()
 _channel.exchange_declare(exchange=_EXCHANGE, exchange_type='fanout')
 
-_REFERENCE_PROFILE = {
-    "id": 1,
-    "averageRating": {
-        "action": 0.0,
-        "adventure": 0.0,
-        "animation": 0.0,
-        "children": 0.0,
-        "comedy": 0.0,
-        "crime": 0.0,
-        "documentary": 0.0,
-        "drama": 0.0,
-        "fantasy": 0.0,
-        "film_noir": 0.0,
-        "horror": 0.0,
-        "imax": 0.0,
-        "musical": 0.0,
-        "mystery": 0.0,
-        "romance": 0.0,
-        "sci_fi": 0.0,
-        "short": 0.0,
-        "thriller": 0.0,
-        "war": 0.0,
-        "western": 0.0
-    },
-    "ratedMovies": [
-        {
-            "id": 1,
-            "genre": "action",
-            "rating": 0.0
-        }
-    ],
-    "stats": {
-        "lastActive": 1588615950.216000000
-    }
-}
-
 
 def _user():
-    rated_movies = [{
-        "id": randint(0, 10000),
-        "genre": choice(list(_REFERENCE_PROFILE['averageRating'].keys())),
-        "rating": round(uniform(0.0, 10.0), 1)
-    } for _ in range(randint(1, 100))]
+    user_id, movie_id, rating, last_active = choice(_USER_RATED_MOVIES)
+    genres = _MOVIES_GENRES[movie_id]
 
-    average_rating = {
-        genre: round(mean(list(map(lambda r: r['rating'], ratings))), 1)
-        for genre, ratings in groupby(rated_movies, key=lambda m: m['genre'])
-    }
-
-    return {
-        "id": randint(0, 1000),
-        "averageRating": average_rating,
-        "ratedMovies": rated_movies,
-        "stats": {
-            "lastActive": int(time()) - randint(1, 30)
+    user_response = get(url=f'{_BS_URL}user/{user_id}')
+    if user_response.status_code == 404:
+        rated_movies = [{
+            "id": movie_id,
+            "genre": choice(genres),  # TODO: use list
+            "rating": rating
+        }]
+        average_rating = {
+            genre: rating
+            for genre in genres
         }
-    }
+        return {
+            "id": user_id,
+            "averageRating": average_rating,
+            "ratedMovies": rated_movies,
+            "stats": {
+                "lastActive": last_active
+            }
+        }
+    elif user_response.status_code == 200:
+        print(f'repeat for {user_id}')
+        current_profile = json.loads(user_response.text)
+
+        current_rated_movies: List[dict] = current_profile['ratedMovies']
+        current_rating = list(filter(lambda m: m['id'] == movie_id, current_rated_movies))
+        if len(current_rating) == 0:
+            rated_movies = current_rated_movies + [{
+                "id": movie_id,
+                "genre": choice(genres),  # TODO: use list
+                "rating": rating
+            }]
+        else:
+            rated_movies = list(filter(lambda m: m['id'] != movie_id, current_rated_movies)) + [{
+                "id": movie_id,
+                "genre": choice(genres),  # TODO: use list
+                "rating": rating
+            }]
+
+        average_rating = {
+            genre: round(mean(list(map(lambda r: r['rating'], ratings))), 1)
+            for genre, ratings in groupby(rated_movies, key=lambda m: m['genre'])
+        }
+
+        return {
+            "id": user_id,
+            "averageRating": average_rating,
+            "ratedMovies": rated_movies,
+            "stats": {
+                "lastActive": last_active
+            }
+        }
+
+    else:
+        print(f'error response for user {user_id}: {user_response}')
 
 
 def publish(ids_queue: Queue = None):
@@ -95,12 +101,13 @@ def publish(ids_queue: Queue = None):
         sent = publish_user()
         if ids_queue is not None:
             ids_queue.put(sent)
-        sleep(.05)
+        sleep(.001)
     print('produce end')
 
 
 def publish_user() -> int:
     event = _user()
+    print(event)
     user_id = event["id"]
     _channel.basic_publish(exchange=_EXCHANGE, routing_key='', body=json.dumps(event))
     print(f'Sending update for user {user_id}')
